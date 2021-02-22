11420ee5ab67c266c48c64ff6732c9cad74b125a
Raphael Discky <zundria.putra@gmail.com>
2021-02-22 12:38:24 +0700

    Create global style

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,29 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+// styled components
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  * {
+    text-decoration: none;
+    cursor:none;
+  }
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden
+  }
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +36,22 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: "#000",
+    text: "#fff",
+  }
+
+  const lightTheme = {
+    background: "#fff",
+    text: "#fff",
+  }
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
 }
 
 Layout.propTypes = {
