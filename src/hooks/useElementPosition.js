f6f897f8f6a0e10d6e581f4f4f729be4355c5220
Raphael Discky <zundria.putra@gmail.com>
2021-02-23 20:42:36 +0700

    Create useElementPosition hook

---
+++ src/hooks/useElementPosition.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+
+export default function useElementPosition(el) {
+  function getElement(x, y) {
+    return { x: x, y: y }
+  }
+  const [elementPosition, setElementPosition] = useState(getElement) // return x and y position
+
+  useEffect(() => {
+    function handlePosition() {
+      let element = el.current
+      let x =
+        element.getBoundingClientRect().left +
+        document.documentElement.scrollLeft +
+        element.offsetWidth / 2
+      let y =
+        element.getBoundingClientRect().top +
+        document.documentElement.scrollTop +
+        element.offsetHeight / 2
+      setElementPosition(getElement(x, y))
+    }
+    handlePosition()
+  }, [el])
+
+  return elementPosition
+}
